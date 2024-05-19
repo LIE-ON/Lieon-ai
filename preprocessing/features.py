ad0d7c8abe015a414a9028fba3ab047bc5589402
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-05-19 15:50:30 +0900

    [refactor] revised/added feature extraction method
    1. pitch 추출 알고리즘 추가
    2. f0 추출 알고리즘 로직 변경(librosa 기반 -> pyworld 기반)
    3. 추출 피처들 row 길이 동일하도록 수정

--- preprocessing/features.py
+++ preprocessing/features.py
@@ -3,7 +3,6 @@
 # csv로 굳이 만들 필요는 없음 - 애초에 csv를 불러오는 목적이 Dataframe을 로드하기 위함이니까
 # 합친 후, 바로 Model로 넘길 것
 
-
 import librosa
 import numpy as np
 import pandas as pd
@@ -13,49 +12,49 @@ from scipy.signal import find_peaks, lfilter, hamming
 from scipy.io import wavfile
 from scipy.fftpack import fft
 import os
+import pyworld as pw
 
 
-# Fundamental Frequency (f0) - 검증 완료, 그러나 실시간 처리를 위한 최적화 필요
-def extract_f0(y, sr):
-    f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'))
-    pitch = np.nan_to_num(f0)
-    return pitch
+# MFCC - 검증 완료 (n_mfcc는 추출할 feature vector의 개수, 보통 12, 13, 20 등을 사용)
+def extract_mfcc(y, sr):
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20, n_fft=2048, hop_length=512, fmin=0, fmax=22050)
+    mfcc_df = pd.DataFrame(mfcc.T, columns=[f'MFCC_{i + 1}' for i in range(mfcc.shape[0])]) # DataFrame으로 변환
+    return mfcc_df
+
+# Pitch - 검증 완료
+def extract_pitch(y, sr, n_fft=2048, hop_length=512):
+    S = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length))
+    pitches, magnitudes = librosa.core.piptrack(S=S, sr=sr, n_fft=n_fft, hop_length=hop_length)
+
+    # Get the maximum pitch for each frame
+    pitch = []
+    for t in range(pitches.shape[1]):
+        index = magnitudes[:, t].argmax()
+        pitch_value = pitches[index, t]
+        pitch.append(pitch_value)
+
+    pitch = np.array(pitch)
+    pitch = np.nan_to_num(pitch)
+    # pitch = pad_to_length(pitch, target_rows)  # Ensure the row count matches
+    pitch_df = pd.DataFrame(pitch, columns=['Pitch'])
+    return pitch_df
 
 
-"""
-# Formants using Parselmouth
-def extract_formants_praat(audio_file):
-    try:
-        # 오디오 파일을 Sound 객체로 변환
-        Sound = parselmouth.Sound(audio_file)
+def extract_f0_pyworld(y, sr, frame_period=512 / 44100 * 1000):
+    y = y.astype(np.float64)
 
-        # Formant 추출
-        formant = Sound.to_formant_burg(time_step=0.1)
+    _f0, t = pw.dio(y, sr, frame_period=frame_period)  # Extract the initial fundamental frequency using the DIO algorithm
+    f0 = pw.stonemask(y, _f0, t, sr)  # Refine the F0 estimation using the StoneMask algorithm
 
-        # Pitch 추출
-        pitch = Sound.to_pitch()
-
-        # Formant 추출에 사용된 시간을 데이터프레임으로 만들기
-        df = pd.DataFrame({"times": formant.ts()})
-
-        # F1부터 F5까지의 값을 각 시간에 대해 추출
-        for idx in range(1, 6):  # F1 to F5
-            df[f'F{idx}'] = df['times'].apply(lambda x: formant.get_value_at_time(formant_number=idx, time=x))
-
-        # Pitch값 추출 (F0)
-        df['F0(pitch)'] = df['times'].apply(lambda x: pitch.get_value_at_time(time=x))
-        # 파일 이름 추가
-        df['filename'] = os.path.basename(audio_file)
-
-        return df
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return pd.DataFrame()  # 오류가 발생하면 빈 데이터프레임 반환
-"""
+    # Convert to a DataFrame and ensure the row count matches
+    # f0 = pad_to_length(f0, target_rows)
+    # times = pad_to_length(t, target_rows)
+    f0_df = pd.DataFrame(f0)
+    return f0_df
 
 
 # low level로 구현한 formant 추출 함수 - 검증 완료, 그러나 너무 느림 (Python 기반)
+# TODO : 속도 개선
 def extract_formants_for_frames(audio_file, frame_length=0.025, hop_length=0.01):
     sample_rate, signal = wavfile.read(audio_file)
     if len(signal.shape) == 2:
@@ -97,7 +96,8 @@ def extract_spectral_flux(y, sr):
     S = np.abs(librosa.stft(y))  # STFT 계산
     flux = np.sqrt(np.sum(np.diff(S, axis=1)**2, axis=0))  # 스펙트럼 간의 차이 계산
     flux = np.append(flux, flux[-1])  # row 수 맞추기 위해 마지막 값 복제
-    return flux
+    flux_df = pd.DataFrame(flux, columns=['Spectral_Flux'])
+    return flux_df
 
 
 # Spectral Entropy - 검증 완료
@@ -105,10 +105,12 @@ def extract_spectral_entropy(y, sr, n_fft=2048, hop_length=512):
     S = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length)) ** 2
     ps = S / np.sum(S, axis=0)
     spectral_entropy = -np.sum(ps * np.log(ps + 1e-10), axis=0)
-    return spectral_entropy
+    spectral_entropy_df = pd.DataFrame(spectral_entropy, columns=['Spectral_Entropy'])
+    return spectral_entropy_df
 
 
 # Prosody: Speech Rate and Pause Duration
+# TODO : 함수 작동하도록 수정 필요
 def extract_prosody_features(y, sr, frame_length=0.025, hop_length=0.01):
     # Frame parameters
     n_frame_length = int(frame_length * sr)
@@ -140,15 +142,51 @@ def extract_prosody_features(y, sr, frame_length=0.025, hop_length=0.01):
 
         average_pauses.append(average_pause)
 
-    """
-    return 후 활용 예시 : speech_rates, average_pauses = extract_prosody_features(y, sr)
-    """
-    return np.array(speech_rates), np.array(average_pauses)
+    speech_rates = pd.DataFrame(speech_rates, columns=['Speech_Rate'])
+    average_pauses = pd.DataFrame(average_pauses, columns=['Average_Pause'])
+
+    # return 후 활용 예시 : speech_rates, average_pauses = extract_prosody_features(y, sr)
+    return speech_rates, average_pauses
 
 
-# MFCC - 검증 완료 (n_mfcc는 추출할 feature vector의 개수, 보통 12, 13, 20 등을 사용)
-# n_fft, hop_length, win_length 등의 파라미터 추가 가능 - 성능이 좋지 않으면 이 파라미터 건드려 볼 것
-def extract_mfcc(y, sr):
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20, n_fft=2048, hop_length=512, fmin=0, fmax=22050)
-    mfcc_df = pd.DataFrame(mfcc.T, columns=[f'MFCC_{i + 1}' for i in range(mfcc.shape[0])]) # DataFrame으로 변환
-    return mfcc_df
+"""
+# Fundamental Frequency (f0) - 검증 완료, 그러나 실시간 처리를 위한 최적화 필요
+def extract_f0(y, sr):
+    f0, voiced_flag, voiced_probs = librosa.pyin(y, fmin=librosa.note_to_hz('C2'), fmax=librosa.note_to_hz('C7'))
+    pitch_f0 = np.nan_to_num(f0)
+    pitch_f0_df = pd.DataFrame(pitch_f0, columns=['F0'])
+    return pitch_f0_df
+"""
+
+
+"""
+# Formants using Parselmouth
+def extract_formants_praat(audio_file):
+    try:
+        # 오디오 파일을 Sound 객체로 변환
+        Sound = parselmouth.Sound(audio_file)
+
+        # Formant 추출
+        formant = Sound.to_formant_burg(time_step=0.1)
+
+        # Pitch 추출
+        pitch = Sound.to_pitch()
+
+        # Formant 추출에 사용된 시간을 데이터프레임으로 만들기
+        df = pd.DataFrame({"times": formant.ts()})
+
+        # F1부터 F5까지의 값을 각 시간에 대해 추출
+        for idx in range(1, 6):  # F1 to F5
+            df[f'F{idx}'] = df['times'].apply(lambda x: formant.get_value_at_time(formant_number=idx, time=x))
+
+        # Pitch값 추출 (F0)
+        df['F0(pitch)'] = df['times'].apply(lambda x: pitch.get_value_at_time(time=x))
+        # 파일 이름 추가
+        df['filename'] = os.path.basename(audio_file)
+
+        return df
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return pd.DataFrame()  # 오류가 발생하면 빈 데이터프레임 반환
+"""
