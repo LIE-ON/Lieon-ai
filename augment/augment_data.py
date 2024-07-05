3bb6abeece1e46ff52dff5a5c8541ba95a10e6a2
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-07-05 15:45:31 +0900

    [Feat] algorithm based augmentation
    #2

--- augment/augment_data.py
+++ augment/augment_data.py
@@ -1,7 +1,23 @@
+import os
 import librosa
 import numpy as np
 import soundfile as sf
-from pydub import AudioSegment
+from torch.utils.data import Dataset, DataLoader
+
+
+class AudioDataset(Dataset):
+    def __init__(self, file_paths, sr=44100):
+        self.file_paths = file_paths
+        self.sr = sr
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, idx):
+        file_path = self.file_paths[idx]
+        y, sr = librosa.load(file_path, sr=self.sr)
+        return y, sr, os.path.basename(file_path)
+
 
 def time_stretch(data, rate=1.0):
     """
@@ -14,6 +30,7 @@ def time_stretch(data, rate=1.0):
     """
     return librosa.effects.time_stretch(data, rate=rate)
 
+
 def pitch_shift(data, sr, n_steps):
     """
     오디오 파일의 피치를 변경합니다. 재생 속도는 그대로 유지됩니다.
@@ -24,6 +41,7 @@ def pitch_shift(data, sr, n_steps):
     :return: 피치 변형된 오디오 데이터.
     """
     return librosa.effects.pitch_shift(data, sr=sr, n_steps=n_steps)
+
 
 def add_noise(data, noise_factor=0.005):
     """
@@ -36,53 +54,44 @@ def add_noise(data, noise_factor=0.005):
     augmented_data = data + noise_factor * noise
     return augmented_data
 
-def time_warp(data, time_warping_factor=1.0):
-    """
-    오디오 파일의 특정 구간을 늘리거나 줄여서 다양한 발음 속도를 시뮬레이션합니다.
-    volume_factor가 0.0보다 작으면 볼륨이 줄어들고, 1.0보다 크면 볼륨이 커집니다. 예를 들어, volume_factor=1.5는 볼륨을 50% 높입니다.
-    :param data: 변형할 오디오 데이터 배열.
-    :param sampling_rate: 오디오의 샘플링 레이트.
-    :param time_warping_factor: 시간 왜곡 비율 (default: 1.0).
-    :return: 시간 왜곡된 오디오 데이터.
-    """
-    time_steps = np.arange(len(data))
-    warped_time_steps = np.interp(time_steps,
-                                  np.linspace(0, len(data), int(len(data) * time_warping_factor)),
-                                  time_steps)
-    return np.interp(warped_time_steps, time_steps, data)
 
-def change_volume(data, volume_factor=1.0):
-    """
-    오디오 파일의 전체 볼륨을 조절합니다. 이는 다양한 음량 환경에서 모델이 잘 작동하도록 만듭니다.
-    :param data: 변형할 오디오 데이터 배열.
-    :param volume_factor: 볼륨 변화 비율 (default: 1.0).
-    :return: 볼륨이 조절된 오디오 데이터.
-    """
-    audio_segment = AudioSegment(
-        data.tobytes(),
-        frame_rate=sampling_rate,
-        sample_width=data.dtype.itemsize,
-        channels=1
-    )
-    return np.array(audio_segment + volume_factor)
+def augment_and_save(data, sr, base_filename, output_dir):
+    # augment data 1
+    tsf_and_addnoise = time_stretch(data, rate=1.3)
+    tsf_and_addnoise = add_noise(tsf_and_addnoise, noise_factor=0.025)
+    sf.write(os.path.join(output_dir, f'{base_filename}_augment_1.wav'), tsf_and_addnoise, sr)
+
+    # augment data 2
+    tss_and_addnoise = time_stretch(data, rate=0.7)
+    tss_and_addnoise = add_noise(tss_and_addnoise, noise_factor=0.05)
+    sf.write(os.path.join(output_dir, f'{base_filename}_augment_2.wav'), tss_and_addnoise, sr)
+
+    # augment data 3
+    psh = pitch_shift(data, sr, n_steps=2)
+    sf.write(os.path.join(output_dir, f'{base_filename}_augment_3.wav'), psh, sr)
+
+    # augment data 4
+    psl = pitch_shift(data, sr, n_steps=-2)
+    sf.write(os.path.join(output_dir, f'{base_filename}_augment_4.wav'), psl, sr)
 
 
-"""
-# Example of loading an audio file
-file_path = 'path_to_audio_file.wav'
-data, sr = librosa.load(file_path)
+def main():
+    input_dir = '/Users/imdohyeon/Documents/PythonWorkspace/Lieon-ai/dataset/train'
+    output_dir = '/Users/imdohyeon/Documents/PythonWorkspace/Lieon-ai/dataset/augment'
+    os.makedirs(output_dir, exist_ok=True)
 
-# Apply augmentations
-augmented_data_time_stretch = time_stretch(data, rate=1.1)
-augmented_data_pitch_shift = pitch_shift(data, sr, n_steps=2)
-augmented_data_noise = add_noise(data, noise_factor=0.01)
-augmented_data_time_warp = time_warp(data, sr, time_warping_factor=1.1)
-augmented_data_volume = change_volume(data, volume_factor=5.0)
+    file_paths = [os.path.join(root, file)
+                  for root, _, files in os.walk(input_dir)
+                  for file in files if file.endswith('.wav')]
 
-# Save augmented data
-sf.write('augmented_time_stretch.wav', augmented_data_time_stretch, sr)
-sf.write('augmented_pitch_shift.wav', augmented_data_pitch_shift, sr)
-sf.write('augmented_noise.wav', augmented_data_noise, sr)
-sf.write('augmented_time_warp.wav', augmented_data_time_warp, sr)
-sf.write('augmented_volume.wav', augmented_data_volume, sr)
-"""
+    dataset = AudioDataset(file_paths)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=4)
+
+    for data, sr, base_filename in dataloader:
+        data = data[0].numpy()  # Batch size is 1, so get the first element and convert to numpy array
+        sr = sr[0].item()  # Unpack the batch
+        base_filename = base_filename[0]  # Unpack the batch
+        augment_and_save(data, sr, base_filename, output_dir)
+
+if __name__ == '__main__':
+    main()
